43742894383a2d1820c0f1ab7ee1374eb643fb6e
serCJm <scjmoro@gmail.com>
2023-10-10 16:21:57 -0700

    Add browsePage module

---
+++ src/modules/browsePage.ts
@@ -0,0 +1,21 @@
+import { Browser } from "puppeteer-core";
+import { MODULES_CONFIG } from "../../config/modules-config.js";
+import { randomNumber } from "../utils/utils.js";
+import { XPup } from "../utils/xpup.js";
+
+export async function browsePage(browser: Browser) {
+	console.log("### Starting to browse ###");
+	try {
+		const page = await browser.newPage();
+
+		await page.goto("https://twitter.com/home");
+
+		const xPage = new XPup(page);
+		const [min, max] = MODULES_CONFIG[browsePage.name].MIN_MAX_BROWSE_TIME;
+		await xPage.humanInfiniteScroll(randomNumber(min, max));
+
+		await xPage.moveMouseOutOfPage();
+	} catch (err) {
+		console.log(err);
+	}
+}
