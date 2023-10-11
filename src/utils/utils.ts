9e7607e661de2574b16db78b3bcfee78c1044dae
serCJm <scjmoro@gmail.com>
2023-10-10 21:45:32 -0700

    Add shuffleArr

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,11 @@
 export function randomNumber(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function shuffleArr([...arr]: any[]) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+}
