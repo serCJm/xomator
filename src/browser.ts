80fa93b66fcb218a8121d9aceb35796928bdd62d
serCJm <scjmoro@gmail.com>
2023-10-13 10:43:24 -0700

    Add random window size

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,6 @@
 import { config } from "../config/config.js";
 import { Profile } from "./types.js";
+import { randomNumber } from "./utils/utils.js";
 
 const { ADS_BROWSER_URL } = config;
 
@@ -14,7 +15,23 @@ export async function getBrowserProfiles(): Promise<Profile[]> {
 }
 
 export async function getBrowserWSEndpoint(userId: string) {
-	const url = `${ADS_BROWSER_URL}browser/start?user_id=${userId}`;
+	const windowSizes = [
+		"1024x768",
+		"1280x800",
+		"1366x768",
+		"1440x900",
+		"1600x900",
+		"1920x1080",
+		"2560x1440",
+	];
+	const randomSize = windowSizes[randomNumber(0, windowSizes.length - 1)];
+	const launchArgs = [`--window-size=${randomSize}`];
+	const encodedLaunchArgs = encodeURIComponent(JSON.stringify(launchArgs));
+
+	const url = `${ADS_BROWSER_URL}browser/start?user_id=${userId}&launch_args=${encodedLaunchArgs}`;
+
+	console.log(url);
+
 	const res: any = await fetch(url);
 	const data = await res.json();
 	if (data.code === 0 && data.data.ws && data.data.ws.puppeteer) {
